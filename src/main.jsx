5af77ce0b9eed00a47661cf7255711050b4dccf2
Yu-Lun Chiang <36063123+allenyummy@users.noreply.github.com>
2024-03-12 21:55:51 +0800

    feat: set up react app and router (#1)

---
+++ src/main.jsx
@@ -0,0 +1,27 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+
+import Login from './pages/login';
+import Home from './pages/home';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/home",
+    element: <Home />,
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
