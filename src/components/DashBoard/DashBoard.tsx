f9f2a4c6f4f6f7cda61e50387b32776de9d87a89
Yu-Lun Chiang <chiangyulun0914@gmail.com>
2024-03-20 23:58:57 +0800

    feat: dashboard

--- src/components/DashBoard/DashBoard.tsx
+++ src/components/DashBoard/DashBoard.tsx
@@ -1,7 +1,15 @@
+import './DashBoard.css';
+
 const DashBoard = () => {
   return (
-    <div>
-      <h1>DashBoard</h1>
+    <div className="dash-board-container">
+      <div className="header">
+        <div className="title">DashBoard</div>
+      </div>
+
+      <div className="body">
+        <div>fdsf</div>
+      </div>
     </div>
   );
 };
