de083a84bd4b6c86441fec9cdd86199631df07bd
Yu-Lun Chiang <chiangyulun0914@gmail.com>
2024-03-23 14:41:54 +0800

    feat: dynamic grid

--- src/components/DashBoard/DashBoard.tsx
+++ src/components/DashBoard/DashBoard.tsx
@@ -1,7 +1,12 @@
+import { useReducer, useState } from 'react';
+
 import './DashBoard.css';
-import { bodyArrangement } from './utils';
+import { BodyArrangementActionType, bodyArrangement, bodyArrangementReducer, dynamicBodyGridStyle } from './utils';
 
 const DashBoard = () => {
+  const [bodyGridStyle, setBodyGridStyle] = useState('default');
+  const [bodyArrangmentState, dispatchBodyArrangement] = useReducer(bodyArrangementReducer, bodyArrangement);
+
   return (
     <div className="dash-board-container">
       <div className="header">
@@ -9,11 +14,24 @@ const DashBoard = () => {
         <div className="update-time">2024-03-24 11:34</div>
       </div>
 
-      <div className="body">
-        {bodyArrangement.map(({ Component, style }, index) => {
+      <div className="body" style={dynamicBodyGridStyle(bodyGridStyle)}>
+        {bodyArrangmentState.map(({ keyIndex, Component, style, attribute }, index) => {
           return (
             <div key={index} style={style}>
-              <Component />
+              <Component
+                expanded={attribute.expanded}
+                onClickExpandButton={(expanded: boolean) => {
+                  dispatchBodyArrangement({
+                    type: BodyArrangementActionType.ATTRIBUTE_CHANGED,
+                    payload: { keyIndex, attribute: { expanded } },
+                  });
+                  if (!expanded) {
+                    setBodyGridStyle('default');
+                  } else {
+                    setBodyGridStyle(keyIndex);
+                  }
+                }}
+              />
             </div>
           );
         })}
--- src/components/DashBoard/utils/bodyArrangement.ts
+++ src/components/DashBoard/utils/bodyArrangement.ts
@@ -9,45 +9,53 @@ import TotalPL from '../components/TotalPL/TotalPL';
 
 interface BodyArrangement {
   [index: string]: unknown;
-  Component: () => JSX.Element;
+  keyIndex: string;
+  Component: (args: any) => JSX.Element;
   style: CSSProperties;
+  attribute: Record<string, unknown>;
 }
 
 export const bodyArrangement: BodyArrangement[] = [
   {
+    keyIndex: 'totalPL',
     Component: TotalPL,
     style: {
-      gridArea: 'grid1',
+      gridArea: 'totalPL',
     },
   },
   {
+    keyIndex: 'calendarPL',
     Component: CalendarPL,
     style: {
-      gridArea: 'grid2',
+      gridArea: 'calendarPL',
     },
   },
   {
+    keyIndex: 'calendarDividend',
     Component: CalendarDividend,
     style: {
-      gridArea: 'grid3',
+      gridArea: 'calendarDividend',
     },
   },
   {
+    keyIndex: 'recentTransaction',
     Component: RecentTransaction,
     style: {
-      gridArea: 'grid4',
+      gridArea: 'recentTransaction',
     },
   },
   {
+    keyIndex: 'timeSeries',
     Component: TimeSeries,
     style: {
-      gridArea: 'grid5',
+      gridArea: 'timeSeries',
     },
   },
   {
+    keyIndex: 'pivotTable',
     Component: PivotTable,
     style: {
-      gridArea: 'grid6',
+      gridArea: 'pivotTable',
     },
   },
 ].map((item) => {
@@ -63,6 +71,125 @@ export const bodyArrangement: BodyArrangement[] = [
       height: '100%',
       minHeight: '100%',
       overflow: 'auto',
+      visibility: 'visible',
+    },
+    attribute: {
+      expanded: false,
     },
   };
 });
+
+export enum BodyArrangementActionType {
+  ATTRIBUTE_CHANGED = 'attribute-changed',
+}
+
+interface BodyArrangementAttributeChangedAction {
+  type: BodyArrangementActionType.ATTRIBUTE_CHANGED;
+  payload: {
+    keyIndex: string;
+    attribute: Record<string, unknown>;
+  };
+}
+
+export const bodyArrangementReducer = (state: BodyArrangement[], action: BodyArrangementAttributeChangedAction) => {
+  switch (action.type) {
+    case BodyArrangementActionType.ATTRIBUTE_CHANGED:
+      const { keyIndex, attribute } = action.payload;
+      const nextState = state.map((item) => {
+        if (item.keyIndex === keyIndex) {
+          return {
+            ...item,
+            style: { ...item.style, visibility: 'visible' },
+            attribute: { ...item.attribute, ...attribute },
+          } as BodyArrangement;
+        }
+        return {
+          ...item,
+          style: { ...item.style, visibility: 'hidden' },
+        } as BodyArrangement;
+      });
+
+      const isAllNotExpanded = nextState.every((item) => !item.attribute.expanded);
+      if (isAllNotExpanded) {
+        return bodyArrangement;
+      }
+      return nextState;
+    default:
+      throw new Error('Unexpected action');
+  }
+};
+
+const bodyGridStyleDefault = {
+  gridTemplateAreas: `'totalPL calendarPL calendarDividend recentTransaction'
+    'timeSeries timeSeries timeSeries recentTransaction'
+    'timeSeries timeSeries timeSeries recentTransaction'
+    'pivotTable pivotTable pivotTable recentTransaction'
+    'pivotTable pivotTable pivotTable recentTransaction'`,
+};
+
+const bodyGridStyleA = {
+  gridTemplateAreas: `'totalPL totalPL totalPL totalPL'
+    'totalPL totalPL totalPL totalPL'
+    'totalPL totalPL totalPL totalPL'
+    'totalPL totalPL totalPL totalPL'
+    'totalPL totalPL totalPL totalPL'`,
+};
+
+const bodyGridStyleB = {
+  gridTemplateAreas: `'calendarPL calendarPL calendarPL calendarPL'
+    'calendarPL calendarPL calendarPL calendarPL'
+    'calendarPL calendarPL calendarPL calendarPL'
+    'calendarPL calendarPL calendarPL calendarPL'
+    'calendarPL calendarPL calendarPL calendarPL'`,
+};
+
+const bodyGridStyleC = {
+  gridTemplateAreas: `'calendarDividend calendarDividend calendarDividend calendarDividend'
+    'calendarDividend calendarDividend calendarDividend calendarDividend'
+    'calendarDividend calendarDividend calendarDividend calendarDividend'
+    'calendarDividend calendarDividend calendarDividend calendarDividend'
+    'calendarDividend calendarDividend calendarDividend calendarDividend'`,
+};
+
+const bodyGridStyleD = {
+  gridTemplateAreas: `'timeSeries timeSeries timeSeries timeSeries'
+    'timeSeries timeSeries timeSeries timeSeries'
+    'timeSeries timeSeries timeSeries timeSeries'
+    'timeSeries timeSeries timeSeries timeSeries'
+    'timeSeries timeSeries timeSeries timeSeries'`,
+};
+
+const bodyGridStyleE = {
+  gridTemplateAreas: `'pivotTable pivotTable pivotTable pivotTable'
+    'pivotTable pivotTable pivotTable pivotTable'
+    'pivotTable pivotTable pivotTable pivotTable'
+    'pivotTable pivotTable pivotTable pivotTable'
+    'pivotTable pivotTable pivotTable pivotTable'`,
+};
+
+const bodyGridStyleF = {
+  gridTemplateAreas: `'recentTransaction recentTransaction recentTransaction recentTransaction'
+    'recentTransaction recentTransaction recentTransaction recentTransaction'
+    'recentTransaction recentTransaction recentTransaction recentTransaction'
+    'recentTransaction recentTransaction recentTransaction recentTransaction'
+    'recentTransaction recentTransaction recentTransaction recentTransaction'`,
+};
+
+export const dynamicBodyGridStyle = (keyIndex: string) => {
+  switch (keyIndex) {
+    case 'totalPL':
+      return bodyGridStyleA;
+    case 'calendarPL':
+      return bodyGridStyleB;
+    case 'calendarDividend':
+      return bodyGridStyleC;
+    case 'recentTransaction':
+      return bodyGridStyleD;
+    case 'timeSeries':
+      return bodyGridStyleE;
+    case 'pivotTable':
+      return bodyGridStyleF;
+    default:
+      return bodyGridStyleDefault;
+  }
+};
