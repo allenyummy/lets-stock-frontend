e5f383a5149974bc05f9c5e80d993692302cb0c8
Yu-Lun Chiang <36063123+allenyummy@users.noreply.github.com>
2024-03-23 19:12:25 +0800

    style: dashboard transition (#20)

--- src/components/DashBoard/components/PivotTable/PivotTable.tsx
+++ src/components/DashBoard/components/PivotTable/PivotTable.tsx
@@ -1,5 +1,25 @@
-const PivotTable = () => {
-  return <div>PivotTable</div>;
+import { IoExpandOutline } from 'react-icons/io5';
+
+import './PivotTable.css';
+
+interface Props {
+  expanded: boolean;
+  onClickExpandButton: (value: boolean) => void;
+}
+
+const PivotTable = ({ expanded, onClickExpandButton }: Props) => {
+  return (
+    <div className="pivot-table-container">
+      <div className="header">
+        <div className="title">Pivot Table</div>
+        <div className="icon">
+          <IoExpandOutline size={'1rem'} onClick={() => onClickExpandButton(!expanded)} />
+        </div>
+      </div>
+
+      <div className="body">Body</div>
+    </div>
+  );
 };
 
 export default PivotTable;
--- src/components/DashBoard/components/RecentTransaction/RecentTransaction.tsx
+++ src/components/DashBoard/components/RecentTransaction/RecentTransaction.tsx
@@ -1,5 +1,25 @@
-const RecentTransaction = () => {
-  return <div>RecentTransaction</div>;
+import { IoExpandOutline } from 'react-icons/io5';
+
+import './RecentTransaction.css';
+
+interface Props {
+  expanded: boolean;
+  onClickExpandButton: (value: boolean) => void;
+}
+
+const RecentTransaction = ({ expanded, onClickExpandButton }: Props) => {
+  return (
+    <div className="recent-transaction-container">
+      <div className="header">
+        <div className="title">Recent Transaction</div>
+        <div className="icon">
+          <IoExpandOutline size={'1rem'} onClick={() => onClickExpandButton(!expanded)} />
+        </div>
+      </div>
+
+      <div className="body">Body</div>
+    </div>
+  );
 };
 
 export default RecentTransaction;
