0870c7c79c874b6db4959bc8eeb2cac9da6bbe9f
Yu-Lun Chiang <chiangyulun0914@gmail.com>
2024-03-23 19:11:27 +0800

    style: dashboard transition

--- src/components/DashBoard/components/TimeSeries/TimeSeries.tsx
+++ src/components/DashBoard/components/TimeSeries/TimeSeries.tsx
@@ -1,5 +1,25 @@
-const TimeSeries = () => {
-  return <div>TimeSeries</div>;
+import { IoExpandOutline } from 'react-icons/io5';
+
+import './TimeSeries.css';
+
+interface Props {
+  expanded: boolean;
+  onClickExpandButton: (value: boolean) => void;
+}
+
+const TimeSeries = ({ expanded, onClickExpandButton }: Props) => {
+  return (
+    <div className="time-series-container">
+      <div className="header">
+        <div className="title">Time Series</div>
+        <div className="icon">
+          <IoExpandOutline size={'1rem'} onClick={() => onClickExpandButton(!expanded)} />
+        </div>
+      </div>
+
+      <div className="body">Body</div>
+    </div>
+  );
 };
 
 export default TimeSeries;
