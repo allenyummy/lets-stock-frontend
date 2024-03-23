de34ed4abef915641b688fd0edeb543efb5242af
Yu-Lun Chiang <36063123+allenyummy@users.noreply.github.com>
2024-03-23 11:41:08 +0800

    feat: dashboard chart (#15)
    * feat: dashboard chart
    
    * feat: total pl
    
    * feat: total pl
    
    * feat: total pl
    
    * feat: router index

--- src/components/DashBoard/components/TotalPL/TotalPL.tsx
+++ src/components/DashBoard/components/TotalPL/TotalPL.tsx
@@ -1,5 +1,80 @@
+import {
+  ArcElement,
+  CategoryScale,
+  Chart as ChartJS,
+  Legend,
+  LineController,
+  LineElement,
+  LinearScale,
+  PointElement,
+  Title,
+  Tooltip,
+} from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
+import { IoExpandOutline } from 'react-icons/io5';
+
+import './TotalPL.css';
+
+ChartJS.register(
+  CategoryScale,
+  Legend,
+  Tooltip,
+  ArcElement,
+  LineController,
+  LineElement,
+  PointElement,
+  LinearScale,
+  Title,
+);
+
+const data = {
+  labels: ['Red', 'Blue', 'Yellow'],
+  datasets: [
+    {
+      label: '嘿嘿',
+      data: [300, 50, 100],
+      backgroundColor: ['rgb(255, 99, 132)', 'rgb(54, 162, 235)', 'rgb(255, 205, 86)'],
+    },
+  ],
+};
+
+const options = {
+  maintainAspectRatio: false,
+  plugins: {
+    legend: {
+      display: false,
+    },
+  },
+};
+
 const TotalPL = () => {
-  return <div>TotalPL</div>;
+  return (
+    <div className="total-pl-container">
+      <div className="header">
+        <div className="title">Total Profit & Loss</div>
+        <div className="icon">
+          <IoExpandOutline />
+        </div>
+      </div>
+
+      <div className="body">
+        <div className="canvas">
+          <Doughnut data={data} options={options} />
+        </div>
+        <div className="description">
+          <div className="percentage">+ 10%</div>
+          <div className="number">
+            <div className="label">市值</div>
+            <div className="value">222</div>
+          </div>
+          <div className="number">
+            <div className="label">成本</div>
+            <div className="value">111</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default TotalPL;
